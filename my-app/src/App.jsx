ba3b6e21c4a9d45e002cea1f27c382e9274c0ade
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-01 17:09:21 +0200

    ejercicio 4

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,11 +2,14 @@ import { HelloWorld } from "./HelloWorld"
 import {Welcome} from "./Welcome"
 
 export function App() {
+    const name = <span>Rodolfo</span>;
     return (
         <div>
             <HelloWorld />
-            <Welcome />
-            {/* Si no pasamos ninguna variable nuestro componente no carga ningun nombre */}
+            <Welcome name={name}/>
+            {/* Si no pasamos ninguna variable nuestro componente no carga ningun nombre */ }
+            {/* En react se puede pasar cualquier expresión javascript como prop, por lo que 
+            simplemente renderizará la expresión que le siempre que se ecuentre dentro de {} */}
         </div>
     )
 }
