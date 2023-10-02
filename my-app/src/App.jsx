68741f8c31472281ac5e35f5b79d339e83540c1d
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-02 03:53:11 +0200

    ejercicio 14 components

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,5 @@
 import { Hello } from "./Hello";
+import { Message } from "./Message";
 
 export function App() {
     return (
@@ -6,7 +7,10 @@ export function App() {
         <Hello />
         <Hello />
         <Hello />
-        {/* Si, puedo usarlo más de una vez y si lo hago, este se repite, repitiendo a su vez  el componente Message */}
+        {/* Si, puedo usarlo más de una vez y si lo hago, este se repite, repitiendo a su vez  el componente Message */ }
+        <Message />
+        {/* Si, puedo renderizar el componente Message directamente dentro de el componente App . Carga su contenido sin necesidad
+        de que cargue también el texto de "Hello World"*/}
       </div>
     );
   }
