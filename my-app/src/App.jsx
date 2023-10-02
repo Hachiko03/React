13ec2e8f4237f029f701394a0ec551ae1fb9b529
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-02 03:39:07 +0200

    ejercicio 13 components

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,6 +4,9 @@ export function App() {
     return (
       <div>
         <Hello />
+        <Hello />
+        <Hello />
+        {/* Si, puedo usarlo más de una vez y si lo hago, este se repite, repitiendo a su vez  el componente Message */}
       </div>
     );
   }
