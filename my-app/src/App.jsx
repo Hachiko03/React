a9e20daca4c0760923fab044f9ef1069db0b0fef
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-02 04:30:37 +0200

    ejercicio 17 props

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -12,7 +12,9 @@ export function App() {
         <Message />
         {/* Si, puedo renderizar el componente Message directamente dentro de el componente App . Carga su contenido sin necesidad
         de que cargue también el texto de "Hello World"*/}
-        <Welcome name="Miguel"/>
+        <Welcome />
+        {/* Si no paso ningún nombre al componente Welcome, este renderizara solo el texto "Welcome, !
+        Pero si le doy un valor predeterminado, en este caso Raul, renderiza ese nombre" */}
       </div>
     );
   }
--- my-app/src/Welcome.jsx
+++ my-app/src/Welcome.jsx
@@ -1,3 +1,3 @@
-export function Welcome({name}) {
+export function Welcome({name = "Raúl"}) {
     return <p>Welcome, {name}!</p>;
   }
