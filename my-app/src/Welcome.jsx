a413a43679c465cb17568ccdff3dc29df14991b4
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-03 17:47:23 +0200

    Conditional Rendering 3

--- my-app/src/Welcome.jsx
+++ my-app/src/Welcome.jsx
@@ -4,7 +4,7 @@ export function Welcome({ name = "Raúl", age }) {
   return (
     <div>
           <p>Welcome, { name }!</p>
-          {age && <Age age={age} />}
+          {age > 18 && age < 65 && <Age age={age} />}
     </div>
   );
 }
