14f62347dd54b7794fc1ba97ace71644e634e582
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-09 19:23:27 +0200

    Events 3

--- my-app/src/AlertClock.jsx
+++ my-app/src/AlertClock.jsx
@@ -1,11 +1,7 @@
-export function AlertClock() {
-    function giveLocalDate() {
-        const hour = new Date()
-        alert (`La hora actual es ${hour.toLocaleTimeString()}`)
-    }
+export function AlertClock({onClick}) {
     return (
         <div>
-            <button onClick={giveLocalDate}> Click para obtener la hora actual</button>
+            <button onClick={onClick}> Click para obtener la hora actual</button>
         </div>
     )
 }
--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,23 +5,28 @@ import { Welcome } from "./Welcome";
 
 export function App() {
   const name = <strong>Rodolfo</strong>;
-    return (
-      <div>
-        <Hello />
-        <Hello />
-        <Hello />
-        {/* Si, puedo usarlo más de una vez y si lo hago, este se repite, repitiendo a su vez  el componente Message */ }
-        <Message />
-        {/* Si, puedo renderizar el componente Message directamente dentro de el componente App . Carga su contenido sin necesidad
+  return (
+    <div>
+      <Hello />
+      <Hello />
+      <Hello />
+      {/* Si, puedo usarlo más de una vez y si lo hago, este se repite, repitiendo a su vez  el componente Message */}
+      <Message />
+      {/* Si, puedo renderizar el componente Message directamente dentro de el componente App . Carga su contenido sin necesidad
         de que cargue también el texto de "Hello World"*/}
-        <Welcome age={5} />
-        {/* Si no paso ningún nombre al componente Welcome, este renderizara solo el texto "Welcome, !
+      <Welcome age={5} />
+      {/* Si no paso ningún nombre al componente Welcome, este renderizara solo el texto "Welcome, !
         Pero si le doy un valor predeterminado, en este caso Raul, renderiza ese nombre" */}
-        <Welcome name={ name } age={ 19 } />
-        {/* Si el nombre es una expresión JSX la renderiza de igual forma, ya que admite como prop cualquier expresión javascrip o 
+      <Welcome name={name} age={19} />
+      {/* Si el nombre es una expresión JSX la renderiza de igual forma, ya que admite como prop cualquier expresión javascrip o 
         JSX. Para pasarla, debemos introducir la expresión JSX dentro de {} */}
-        <AlertClock/>
-      </div>
-    );
-  }
-  export default App;
+      <AlertClock
+        onClick={function giveLocalDate() {
+          const hour = new Date();
+          alert(`La hora actual es ${hour.toLocaleTimeString()}`);
+        }}
+      />
+    </div>
+  );
+}
+export default App;
