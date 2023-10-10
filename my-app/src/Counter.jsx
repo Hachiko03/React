805dcf520242a0b354bacfa809b40d73fb3b1d53
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-10 19:19:04 +0200

    State 2

--- my-app/src/Counter.jsx
+++ my-app/src/Counter.jsx
@@ -12,3 +12,7 @@ export default function Counter() {
     </div>
   )
 }
+/* En este caso pasamos un valor inmediato dentro de la función que react nos da en su useState,
+esto no nos causará problemas aqui porque nuestro contador no depende de su valor anterior. En caso 
+de que nuestro counter si dependiera de nuestro valor anterior o necesitase multiples actualizaciones
+a la vez, tendríamos que pasar una función */
