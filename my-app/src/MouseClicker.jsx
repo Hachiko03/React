17b06f3be049d8473feec3b2003906a33101fa98
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-13 10:42:46 +0200

    Handling Events 2

--- my-app/src/MouseClicker.jsx
+++ my-app/src/MouseClicker.jsx
@@ -1,12 +1,11 @@
-import React, { useEffect } from 'react'
 
 export default function MouseClicker() {
     function printNameOfEventTarget(event) {
-        console.log(event.target.name)
+        console.log(event.currentTarget.name)
     }
   return (
       <div>
-          <button name='one' onClick={printNameOfEventTarget}>Handle Event 1</button>
+          <button name='one' onClick={printNameOfEventTarget}>Handle Event 1 <img src=".\src\images\icons8-animal-25.png"/></button>
     </div>
   )
 }
