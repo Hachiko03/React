551abaef67658166d72c54abedba6d5faa97c318
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-15 17:01:59 +0200

    Uncontrolled Forms 2

--- my-app/src/UncontrolledLogin.jsx
+++ my-app/src/UncontrolledLogin.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 
 export default function UncontrolledLogin() {
+  function submitData(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    const data = {
+      username: formData.get("username"),
+      password: formData.get("password"),
+      remember: formData.get("remember") === 'on' ? true : false,
+      };
+      console.log(data)
+    }
+    /* Se puede acceder a la data del form usando el constructor FormData
+    o bien buscandola el valor accediendo al DOM 
+
+    function submitData(event) {
+    event.preventDefault();
+
+    const data = {
+      username: event.target.elements.namedItem('username').value,
+      password: event.target.elements.namedItem('password').value,
+      remember: event.target.elements.namedItem('remember').checked,
+      };
+      console.log(data)
+    }*/
+
   return (
-    <form>
+    <form onSubmit={submitData}>
       <input name="username" />
-      <input name="passsword" type="password" />
-      <input name="session" type="checkbox" />
+      <input name="password" type="password" />
+      <span>Remember</span>
+      <input name="remember" type="checkbox" />
       <button> Login</button>
       <button type="reset">Reset</button>
     </form>
