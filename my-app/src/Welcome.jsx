5292e36af7a20eea0c31b4e4240b157adfa16f30
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-22 17:02:05 +0200

    Styling Components

--- my-app/src/Welcome.jsx
+++ my-app/src/Welcome.jsx
@@ -2,7 +2,7 @@ import { Age } from "./Age";
 
 export function Welcome({ name = "Raúl", age }) {
   return (
-    <div>
+    <div className="welcome">
           {/* name === "John" && age > 18 && age < 65 ?  */<p>Welcome, { name }!</p> }
           {/* age > 18 && age < 65 &&  */<Age age={age} />}
     </div>
