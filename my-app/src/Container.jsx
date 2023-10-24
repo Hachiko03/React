3f2e0841a2f683edf4727be758228254af719ead
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-24 11:10:29 +0200

    Component Composition 2

--- my-app/src/Container.jsx
+++ my-app/src/Container.jsx
@@ -1,7 +1,8 @@
 import React, { Children } from 'react'
 
-export default function Container({children}) {
+export default function Container({title,children}) {
   return (
-      <div className='container'>{children}</div>
+      <div className='container'>
+          <div>{ title }</div>{ children }</div>
   )
 }
