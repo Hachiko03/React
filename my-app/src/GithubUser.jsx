948420cf6589710b01d6e2866c6f0cd64b8ac9d0
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-26 19:54:17 +0200

    Data Fetching with fetch and useEffect

--- my-app/src/GithubUser.jsx
+++ my-app/src/GithubUser.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 export default function GithubUser({ username }) {
   const [data, setData] = useState(null);
   useEffect(() => {
-    fetch("https://api.github.com/users/Hachiko03")
+    fetch(`https://api.github.com/users/${username}` )
       .then((response) => {
         return response.json()
       })
