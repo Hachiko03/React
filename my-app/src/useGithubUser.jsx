3ed1bfbcf3e6fc01b42f8a9b52509b41743feadd
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-27 11:28:33 +0200

    Custom Hooks 4

--- my-app/src/useGithubUser.jsx
+++ my-app/src/useGithubUser.jsx
@@ -1,18 +1,27 @@
 import { useState, useEffect } from "react";
 
-
 export default function useGithubUser(username) {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   useEffect(() => {
+    setLoading(true);
+    setError(null);
     fetch(`https://api.github.com/users/${username}`)
       .then((response) => {
         return response.json();
       })
       .then((json) => {
+        setLoading(false);
         setData(json);
+      })
+      .catch((error) => {
+        setError(error);
       });
   }, [username]);
   return {
-    data: data,
+      data: data,
+      loading: loading,
+      error: error,
   };
 }
