8ba403fc9513457e04f62cf0e6a4d3aa8a4d0891
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-27 08:22:44 +0200

    Custom Hooks

--- my-app/src/Counter.jsx
+++ my-app/src/Counter.jsx
@@ -1,48 +1,56 @@
 import { useEffect, useRef, useState } from "react";
 import CounterDisplay from "./CounterDisplay";
 
-export default function Counter({ i, aumentar, reducir }) {
-  const [contador, modCounter] = useState(i);
+export default function Counter() {
+  function useCounter(i = 0, aumentar = 1, reducir = 1) {
+    const [contador, modCounter] = useState(i);
 
-  useEffect(() => {
-    console.log(contador)
-  }, [contador]);
-  function aumentarNum() {
-    modCounter(contador + aumentar);
-  }
-  function disminuirNum() {
-    modCounter(contador - reducir)
-  }
-  function resetearNum() {
-    modCounter(i)
-  }
-
-  const refUpDownOfCounter = useRef("")
-  const prevrefUpDownOfCounter = useRef("")
-
-  useEffect(() => {
-    if (contador > i) {
-      refUpDownOfCounter.current = "up";
-    } else if (contador < i ){
-      refUpDownOfCounter.current = "down";
+    useEffect(() => {
+      console.log(contador);
+    }, [contador]);
+    function aumentarNum() {
+      modCounter(contador + aumentar);
+    }
+    function disminuirNum() {
+      modCounter(contador - reducir);
+    }
+    function resetearNum() {
+      modCounter(i);
     }
 
-    if (refUpDownOfCounter.current !== prevrefUpDownOfCounter.current) {
-      console.log(refUpDownOfCounter.current)
-      prevrefUpDownOfCounter.current = refUpDownOfCounter.current;
-    }
-  }, [contador, i])
+    const refUpDownOfCounter = useRef("");
+    const prevrefUpDownOfCounter = useRef("");
+
+    useEffect(() => {
+      if (contador > i) {
+        refUpDownOfCounter.current = "up";
+      } else if (contador < i) {
+        refUpDownOfCounter.current = "down";
+      }
+
+      if (refUpDownOfCounter.current !== prevrefUpDownOfCounter.current) {
+        console.log(refUpDownOfCounter.current);
+        prevrefUpDownOfCounter.current = refUpDownOfCounter.current;
+      }
+    }, [contador, i]);
+    return {
+      contador: contador,
+      aumentar: aumentarNum,
+      disminuir: disminuirNum,
+      resetear: resetearNum,
+    };
+  }
 
   const MyStyle = {
-    backgroundColor: 'lightgreen',
+    backgroundColor: "lightgreen",
   };
-  
+  const { contador, aumentar, disminuir, resetear } = useCounter();
   return (
     <div style={MyStyle}>
-      <CounterDisplay contador={contador}/>
-      <button onClick={ aumentarNum }>Aumentar</button>
-      <button onClick={disminuirNum}>Disminuir</button>
-      <button onClick={resetearNum}>Reset</button>
+      <CounterDisplay contador={contador} />
+      <button onClick={aumentar}>Aumentar</button>
+      <button onClick={disminuir}>Disminuir</button>
+      <button onClick={resetear}>Reset</button>
     </div>
   );
 }
