466b2da2869b3864728e638357fba9c6116b4473
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-28 20:25:37 +0200

    React Router 3

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import { Welcome } from "./Welcome";
 import Counter from "./Counter";
+import ShowGithubUser from "./ShowGithubUser";
+
 
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Welcome name={"Raúl"} />} />
-        <Route path="counter" element={<Counter />} />
+              <Route path="counter" element={ <Counter /> } />
+              <Route path="users/:username" element={<ShowGithubUser/>}></Route>
       </Routes>
     </div>
   );
---
+++ my-app/src/ShowGithubUser.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useParams } from "react-router";
+import GithubUser from "./GithubUser";
+
+export default function ShowGithubUser() {
+  const { username } = useParams();
+  return <GithubUser username={username}/>;
+}
