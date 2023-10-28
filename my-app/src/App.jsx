2489ecd6e51eb7ee37b2eeec62f44a8868c37142
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-28 12:18:12 +0200

    React Router 2

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router";
-import {Welcome} from "./Welcome";
+import { Welcome } from "./Welcome";
+import Counter from "./Counter";
 
 export default function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Welcome name={"Raúl"} />}/>
+        <Route path="/" element={<Welcome name={"Raúl"} />} />
+        <Route path="counter" element={<Counter />} />
       </Routes>
     </div>
   );
