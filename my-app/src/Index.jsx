d928fdc8b8bce41a73d8bdafcab0eab07f3b9175
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-30 07:01:23 +0100

    SWR 4

--- my-app/src/Index.jsx
+++ my-app/src/Index.jsx
@@ -1,11 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App'
-
+import { SWRConfig } from 'swr';
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <SWRConfig value={{fetcher}}>
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  
+  </SWRConfig>
 );
--- my-app/src/useGithubUser.jsx
+++ my-app/src/useGithubUser.jsx
@@ -1,7 +1,5 @@
 import useSWR from "swr";
 
-const fetcher = (url) => fetch(url).then((response) => response.json());
-
 export default function useGithubUser(username) {
   if (username === null) {
     return {
@@ -11,8 +9,7 @@ export default function useGithubUser(username) {
     };
   }
   const { data, error, mutate } = useSWR(
-    `https://api.github.com/users/${username}`,
-    fetcher
+    `https://api.github.com/users/${username}`
   );
 
   function refetch() {
