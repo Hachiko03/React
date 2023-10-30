576949a98788d768f9282e052f36dd247d919872
Jeniffer Flores <hachikofu03@gmail.com>
2023-10-30 06:52:41 +0100

    SWR 2

--- my-app/src/useGithubUser.jsx
+++ my-app/src/useGithubUser.jsx
@@ -1,12 +1,22 @@
 import useSWR from "swr";
 
-const fetcher = (url) => fetch(url).then((response)=>response.json())
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function useGithubUser(username) {
-  const {data, error} = useSWR((`https://api.github.com/users/${username}`),fetcher)
+  if (username === null) {
+    return {
+      data: null,
+      loading: false,
+      error: null,
+    };
+  }
+  const { data, error } = useSWR(
+    `https://api.github.com/users/${username}`,
+    fetcher
+  );
   return {
-      data: data,
-      loading: !data && !error,
-      error: error,
+    data: data,
+    loading: !data && !error,
+    error: error,
   };
 }
